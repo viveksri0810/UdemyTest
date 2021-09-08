600fec9a2924509e5b84d1ada8cccce75acd099c
vivek <viveksri0810@gmail.com>
2021-09-09 03:43:45 +0530

    added complex number class

--- src/Floor.java
+++ src/Floor.java
@@ -1,3 +1,54 @@
 public class Floor {
 
+    private double length;
+    private double width;
+
+    public Floor(double length, double width) {
+        if(width<0){
+            this.width=0;
+        }else {
+            this.width = width;
+        }
+        if(length<0){
+            this.length=0;
+        }else {
+            this.length = length;
+        }
+    }
+    public double getArea(){
+        return (this.length * this.width);
+    }
+}
+
+
+class Carpet{
+
+    private double cost;
+
+    public Carpet(double cost) {
+        if(this.cost<0){
+            this.cost=0;
+        }else{
+            this.cost = cost;
+        }
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
+
+class Calculator{
+
+    Floor floor;
+    Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost(){
+        return floor.getArea()*carpet.getCost();
+    }
 }
