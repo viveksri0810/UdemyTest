2290d141b476279292fcc0885fb4478fb4b778b0
vivek <viveksri0810@gmail.com>
2021-09-09 01:07:17 +0530

    added Floor/Point/Wall class

--- src/Wall.java
+++ src/Wall.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class Wall {
+public class Wall {
+    double width;
+    double height;
+
+    public Wall(){
+
+    }
+    public Wall(double width, double height){
+        if (width<=0){
+            this.width=0;
+        }
+        else{
+            this.width=width;
+        }
+        if(height<=0){
+            this.height=0;
+        }
+        else{
+            this.height=height;
+        }
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if (width<=0){
+            this.width=0;
+        }
+        else{
+            this.width=width;
+        }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        if(height<=0){
+            this.height=0;
+        }
+        else{
+            this.height=height;
+        }
+    }
+
+    public double getArea(){
+        return this.height*this.width;
+    }
+
 }
