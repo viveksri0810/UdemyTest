be5d80afbe373ae1339da2a5357610e12097d418
vivek <viveksri0810@gmail.com>
2021-09-26 10:36:51 +0530

    adding GroceryList.java file.

---
+++ src/ArrayList/GroceryList.java
@@ -0,0 +1,39 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+
+    private ArrayList<String> groceryList;
+
+    public GroceryList(ArrayList<String> groceryList) {
+        this.groceryList = groceryList;
+    }
+
+
+    public void printGroceryItems(){
+        System.out.println("Printing item from grocery List .....");
+        for (String s : groceryList) {
+            System.out.print(s + " | ");
+        }
+        System.out.println();
+    }
+
+    public void addItem(String item){
+ //     System.out.println("adding item");
+        groceryList.add(item);
+        System.out.println(item + " added.");
+    }
+
+    public void modifyGroceryItem(String name,String name1){
+        int i=groceryList.indexOf(name);
+        if(i<0){
+            System.out.println(name +" not present in list, adding it");
+            addItem(name1);
+        }
+        else {
+            groceryList.set(i, name1);
+            System.out.println(name + " has been modified as " + name1);
+        }
+    }
+}
